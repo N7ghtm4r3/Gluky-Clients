3e8f9ac95a9a626f9e76bca689d4cc77842f0d76
N7ghtm4r3 <maurizio.manuel2003@gmail.com>
2025-05-14 16:36:32 +0200

    Imported the theme

--- composeApp/src/commonMain/kotlin/com/tecknobit/gluky/App.kt
+++ composeApp/src/commonMain/kotlin/com/tecknobit/gluky/App.kt
@@ -1,8 +1,24 @@
 package com.tecknobit.gluky
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.text.font.FontFamily
+import gluky.composeapp.generated.resources.Res
+import gluky.composeapp.generated.resources.comicneue
+import gluky.composeapp.generated.resources.fredoka
+import org.jetbrains.compose.resources.Font
+
+/**
+ * `displayFontFamily` the Gluky's font family
+ */
+lateinit var displayFontFamily: FontFamily
+
+/**
+ * `bodyFontFamily` the Gluky's body font family
+ */
+lateinit var bodyFontFamily: FontFamily
 
 @Composable
 fun App() {
-
+    displayFontFamily = FontFamily(Font(Res.font.fredoka))
+    bodyFontFamily = FontFamily(Font(Res.font.comicneue))
 }
---
+++ composeApp/src/commonMain/kotlin/com/tecknobit/gluky/ui/theme/Theme.kt
@@ -0,0 +1,120 @@
+package com.tecknobit.gluky.ui.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+
+private val lightScheme = lightColorScheme(
+    primary = primaryLight,
+    onPrimary = onPrimaryLight,
+    primaryContainer = primaryContainerLight,
+    onPrimaryContainer = onPrimaryContainerLight,
+    secondary = secondaryLight,
+    onSecondary = onSecondaryLight,
+    secondaryContainer = secondaryContainerLight,
+    onSecondaryContainer = onSecondaryContainerLight,
+    tertiary = tertiaryLight,
+    onTertiary = onTertiaryLight,
+    tertiaryContainer = tertiaryContainerLight,
+    onTertiaryContainer = onTertiaryContainerLight,
+    error = errorLight,
+    onError = onErrorLight,
+    errorContainer = errorContainerLight,
+    onErrorContainer = onErrorContainerLight,
+    background = backgroundLight,
+    onBackground = onBackgroundLight,
+    surface = surfaceLight,
+    onSurface = onSurfaceLight,
+    surfaceVariant = surfaceVariantLight,
+    onSurfaceVariant = onSurfaceVariantLight,
+    outline = outlineLight,
+    outlineVariant = outlineVariantLight,
+    scrim = scrimLight,
+    inverseSurface = inverseSurfaceLight,
+    inverseOnSurface = inverseOnSurfaceLight,
+    inversePrimary = inversePrimaryLight,
+    surfaceDim = surfaceDimLight,
+    surfaceBright = surfaceBrightLight,
+    surfaceContainerLowest = surfaceContainerLowestLight,
+    surfaceContainerLow = surfaceContainerLowLight,
+    surfaceContainer = surfaceContainerLight,
+    surfaceContainerHigh = surfaceContainerHighLight,
+    surfaceContainerHighest = surfaceContainerHighestLight,
+)
+
+private val darkScheme = darkColorScheme(
+    primary = primaryDark,
+    onPrimary = onPrimaryDark,
+    primaryContainer = primaryContainerDark,
+    onPrimaryContainer = onPrimaryContainerDark,
+    secondary = secondaryDark,
+    onSecondary = onSecondaryDark,
+    secondaryContainer = secondaryContainerDark,
+    onSecondaryContainer = onSecondaryContainerDark,
+    tertiary = tertiaryDark,
+    onTertiary = onTertiaryDark,
+    tertiaryContainer = tertiaryContainerDark,
+    onTertiaryContainer = onTertiaryContainerDark,
+    error = errorDark,
+    onError = onErrorDark,
+    errorContainer = errorContainerDark,
+    onErrorContainer = onErrorContainerDark,
+    background = backgroundDark,
+    onBackground = onBackgroundDark,
+    surface = surfaceDark,
+    onSurface = onSurfaceDark,
+    surfaceVariant = surfaceVariantDark,
+    onSurfaceVariant = onSurfaceVariantDark,
+    outline = outlineDark,
+    outlineVariant = outlineVariantDark,
+    scrim = scrimDark,
+    inverseSurface = inverseSurfaceDark,
+    inverseOnSurface = inverseOnSurfaceDark,
+    inversePrimary = inversePrimaryDark,
+    surfaceDim = surfaceDimDark,
+    surfaceBright = surfaceBrightDark,
+    surfaceContainerLowest = surfaceContainerLowestDark,
+    surfaceContainerLow = surfaceContainerLowDark,
+    surfaceContainer = surfaceContainerDark,
+    surfaceContainerHigh = surfaceContainerHighDark,
+    surfaceContainerHighest = surfaceContainerHighestDark,
+)
+
+/**
+ * Method to set the Gluky theme to the content
+ *
+ * @param darkTheme Whether to use the dark or light theme
+ * @param content The content to display
+ */
+@Composable
+fun GlukyTheme(
+    darkTheme: Boolean = applyDarkTheme(),
+    content: @Composable () -> Unit,
+) {
+    val colorScheme = when {
+        darkTheme -> darkScheme
+        else -> lightScheme
+    }
+    MaterialTheme(
+        colorScheme = colorScheme,
+        typography = AppTypography,
+        content = content
+    )
+}
+
+/**
+ * Method to check which colors scheme is to use based on the current [localUser.theme] property
+ *
+ * @return whether use the dark colors scheme as [Boolean]
+ */
+@Composable
+fun applyDarkTheme(): Boolean {
+    // TODO: TO SET
+    /*return when (localUser.theme) {
+        Dark -> true
+        Light -> false*/
+    /*Auto -> */return isSystemInDarkTheme()
+    //}
+}
