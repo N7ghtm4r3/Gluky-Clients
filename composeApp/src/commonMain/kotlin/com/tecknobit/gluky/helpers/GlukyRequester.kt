0a6c5cdc824ca7bcf16d513b18f1056e8d4e40f4
N7ghtm4r3 <maurizio.manuel2003@gmail.com>
2025-06-07 13:51:12 +0200

    Completed the code documentation!

--- composeApp/src/commonMain/kotlin/com/tecknobit/gluky/helpers/GlukyRequester.kt
+++ composeApp/src/commonMain/kotlin/com/tecknobit/gluky/helpers/GlukyRequester.kt
@@ -4,9 +4,13 @@ import androidx.compose.runtime.MutableState
 import com.tecknobit.equinoxcompose.network.EquinoxRequester
 import com.tecknobit.equinoxcore.annotations.Assembler
 import com.tecknobit.equinoxcore.annotations.CustomParametersOrder
+import com.tecknobit.equinoxcore.annotations.RequestPath
 import com.tecknobit.equinoxcore.annotations.Wrapper
 import com.tecknobit.equinoxcore.helpers.LANGUAGE_KEY
 import com.tecknobit.equinoxcore.network.EquinoxBaseEndpointsSet.Companion.BASE_EQUINOX_ENDPOINT
+import com.tecknobit.equinoxcore.network.RequestMethod.DELETE
+import com.tecknobit.equinoxcore.network.RequestMethod.GET
+import com.tecknobit.equinoxcore.network.RequestMethod.PUT
 import com.tecknobit.equinoxcore.time.TimeFormatter.toDateString
 import com.tecknobit.gluky.BACKEND_URL
 import com.tecknobit.gluky.ui.screens.analyses.data.Report
@@ -37,6 +41,19 @@ import kotlinx.serialization.json.jsonObject
 import kotlinx.serialization.json.put
 import kotlinx.serialization.json.putJsonObject
 
+/**
+ * The `GlukyRequester` class is useful to communicate with the Gluky's backend
+ *
+ * @param userId The user identifier
+ * @param userToken The user token
+ * @param debugMode Whether the requester is still in development and who is developing needs the log of the requester's
+ * workflow, if it is enabled all the details of the requests sent and the errors occurred will be printed in the console
+ *
+ * @author N7ghtm4r3 - Tecknobit
+ *
+ * @see com.tecknobit.equinoxcore.network.Requester
+ * @see EquinoxRequester
+ */
 // TODO: TO SETUP CORRECTLY (SSL validation)
 class GlukyRequester(
     userId: String? = null,
@@ -53,11 +70,25 @@ class GlukyRequester(
 
     private companion object {
 
+        /**
+         * `TARGET_DAY_PATTERN` the pattern used to format the dates
+         */
         const val TARGET_DAY_PATTERN = "dd-MM-yyyy"
 
     }
 
+    /**
+     * Method used to create the payload for the [signIn] request
+     *
+     * @param email The email of the user
+     * @param password The password of the user
+     * @param custom The custom parameters added in a customization of the equinox user to execute a customized sign-in
+     *
+     * @return the payload for the request as [JsonObject]
+     *
+     */
     // TODO: TO REMOVE WHEN LANGUAGE PARAMETER IMPLEMENTED BUILT-IN
+    @Assembler
     @CustomParametersOrder(LANGUAGE_KEY)
     override fun getSignInPayload(
         email: String,
@@ -69,6 +100,14 @@ class GlukyRequester(
         return Json.encodeToJsonElement(payload).jsonObject
     }
 
+    /**
+     * Request to get the daily measurement
+     *
+     * @param targetDay The target day
+     *
+     * @return the result of the request as [JsonObject]
+     */
+    @RequestPath(path = "/api/v1/users/{user_id}/measurements/{target_day}", method = GET)
     suspend fun getDailyMeasurements(
         targetDay: Long,
     ): JsonObject {
@@ -79,6 +118,14 @@ class GlukyRequester(
         )
     }
 
+    /**
+     * Request to fill the specified [targetDay]
+     *
+     * @param targetDay The target day to fill
+     *
+     * @return the result of the request as [JsonObject]
+     */
+    @RequestPath(path = "/api/v1/users/{user_id}/measurements/{target_day}", method = PUT)
     suspend fun fillDay(
         targetDay: Long,
     ): JsonObject {
@@ -89,6 +136,22 @@ class GlukyRequester(
         )
     }
 
+    /**
+     * Request to fill a meal
+     *
+     * @param targetDay The target day of the meal to fill
+     * @param mealType The type of the meal to fill
+     * @param glycemia The pre-prendial glycemia value
+     * @param postPrandialGlycemia The post-prendial glycemia value
+     * @param insulinUnits The administrated insulin units
+     * @param content The content of the meal
+     *
+     * @return the result of the request as [JsonObject]
+     */
+    @RequestPath(
+        path = "/api/v1/users/{user_id}/measurements/{target_day}/meals/{type}",
+        method = PUT
+    )
     suspend fun fillMeal(
         targetDay: Long,
         mealType: MeasurementType,
@@ -116,6 +179,14 @@ class GlukyRequester(
         )
     }
 
+    /**
+     * Method used to assemble the endpoint to make the requests related to the meals
+     *
+     * @param targetDay The target day
+     * @param mealType The type of the meal
+     *
+     * @return an endpoint to make the request as [String]
+     */
     @Wrapper
     @Assembler
     private fun assembleMealsUrl(
@@ -132,6 +203,19 @@ class GlukyRequester(
         return mealsUrl
     }
 
+    /**
+     * Request to fill a basal insulin record
+     *
+     * @param targetDay The target day of the basal insulin record to fill
+     * @param glycemia The glycemia value
+     * @param insulinUnits The administrated insulin units
+     *
+     * @return the result of the request as [JsonObject]
+     */
+    @RequestPath(
+        path = "/api/v1/users/{user_id}/measurements/{target_day}/basal_insulin",
+        method = PUT
+    )
     suspend fun fillBasalInsulin(
         targetDay: Long,
         glycemia: String,
@@ -150,6 +234,18 @@ class GlukyRequester(
         )
     }
 
+    /**
+     * Request to save the notes related to the target day
+     *
+     * @param targetDay The target day of the basal insulin record to fill
+     * @param content The content of the daily notes
+     *
+     * @return the result of the request as [JsonObject]
+     */
+    @RequestPath(
+        path = "/api/v1/users/{user_id}/measurements/{target_day}/daily_notes",
+        method = PUT
+    )
     suspend fun saveDailyNotes(
         targetDay: Long,
         content: String,
@@ -166,6 +262,14 @@ class GlukyRequester(
         )
     }
 
+    /**
+     * Method used to assemble the endpoint to make the requests to the measurements controller
+     *
+     * @param targetDay The target day
+     * @param customPath Optional custom path to append
+     *
+     * @return an endpoint to make the request as [String]
+     */
     @Assembler
     private fun assembleMeasurementsUrl(
         targetDay: Long,
@@ -183,6 +287,17 @@ class GlukyRequester(
         )
     }
 
+    /**
+     * Request to retrieve the glycemic trend with the specified parameters
+     *
+     * @param period      The period to respect with the dates range
+     * @param groupingDay The grouping day
+     * @param from        The start date from retrieve the measurements
+     * @param to          The end date to retrieve the measurements
+     *
+     * @return the result of the request as [JsonObject]
+     */
+    @RequestPath(path = "/api/v1/users/{user_id}/analyses", method = GET)
     suspend fun getGlycemicTrend(
         period: GlycemicTrendPeriod,
         groupingDay: GlycemicTrendGroupingDay?,
@@ -201,6 +316,17 @@ class GlukyRequester(
         )
     }
 
+    /**
+     * Request to create a report related to a glycemic trend with the specified parameters
+     *
+     * @param period      The period to respect with the dates range
+     * @param groupingDay The grouping day
+     * @param from        The start date from retrieve the measurements
+     * @param to          The end date to retrieve the measurements
+     *
+     * @return the result of the request as [JsonObject]
+     */
+    @RequestPath(path = "/api/v1/users/{user_id}/analyses/reports", method = GET)
     suspend fun createReport(
         period: GlycemicTrendPeriod,
         groupingDay: GlycemicTrendGroupingDay?,
@@ -221,6 +347,16 @@ class GlukyRequester(
         )
     }
 
+    /**
+     * Method used to create the query with the data related to a period
+     *
+     * @param period      The period to respect with the dates range
+     * @param groupingDay The grouping day
+     * @param from        The start date from retrieve the measurements
+     * @param to          The end date to retrieve the measurements
+     *
+     * @return the query as [JsonObject]
+     */
     @Assembler
     private fun createPeriodQuery(
         period: GlycemicTrendPeriod,
@@ -240,6 +376,14 @@ class GlukyRequester(
         }
     }
 
+    /**
+     * Request to delete a report
+     *
+     * @param report The report to delete
+     *
+     * @return the result of the request as [JsonObject]
+     */
+    @RequestPath(path = "/api/v1/users/{user_id}/analyses/reports/{report_id}", method = DELETE)
     suspend fun deleteReport(
         report: Report,
     ): JsonObject {
@@ -250,6 +394,13 @@ class GlukyRequester(
         )
     }
 
+    /**
+     * Method used to assemble the endpoint to make the requests to the analyses controller
+     *
+     * @param subEndpoint Optional sub-endpoint to append
+     *
+     * @return an endpoint to make the request as [String]
+     */
     @Assembler
     private fun assembleAnalysesUrl(
         subEndpoint: String = "",
@@ -260,6 +411,12 @@ class GlukyRequester(
         )
     }
 
+    /**
+     * Request to download a created request
+     *
+     * @param report The report to download
+     * @param onDownloadCompleted The callback to invoke after the report downloaded
+     */
     suspend fun downloadReport(
         report: Report,
         onDownloadCompleted: (String?) -> Unit,
